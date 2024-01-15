19d1d55db6ef040a090192f5bfc17d8736a3c8ef
voduchuy2001 <voduchuy2001@gmail.com>
2024-01-15 15:32:15 +0700

    chores

--- src/views/OrderList.jsx
+++ src/views/OrderList.jsx
@@ -2,6 +2,7 @@ import axios from '../axios'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { formatCurrencyVND } from '../utils/helper'
+import { IMAGE_BASE_URL } from '../utils/constant'
 
 const OrderList = () => {
   const [data, setData] = useState([])
@@ -20,8 +21,8 @@ const OrderList = () => {
   }
 
   return (
-    <div className="mx-auto max-w-3xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
-      <div className="mx-auto max-w-2xl">
+    <div className="px-4 py-10 sm:px-2 lg:px-8 lg:py-4">
+      <div className="mx-auto max-w-3xl">
         <div className="relative z-10 mt-5 rounded-xl border bg-white p-4 sm:mt-10 md:p-10 dark:border-gray-700 dark:bg-gray-800">
           <div className="flex flex-col">
             <div className="-m-1.5 overflow-x-auto">
@@ -50,6 +51,13 @@ const OrderList = () => {
                         >
                           Details
                         </th>
+
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-start text-xs font-medium uppercase text-gray-500"
+                        >
+                          Total
+                        </th>
                       </tr>
                     </thead>
                     <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
@@ -59,7 +67,7 @@ const OrderList = () => {
                             {data._id}
                           </td>
 
-                          <td className="whitespace-nowrap px-6 py-4 text-sm text-blue-800 dark:text-blue-200 uppercase">
+                          <td className="whitespace-nowrap px-6 py-4 text-sm font-bold uppercase text-blue-800 dark:text-blue-200">
                             {data.status}
                           </td>
 
@@ -67,6 +75,12 @@ const OrderList = () => {
                             {data.products.map((product) => (
                               <div key={product._id}>
                                 <div className="rounded-md border p-4 text-sm text-gray-600 dark:text-gray-400">
+                                  <div className="my-2 h-16 w-16 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <img
+                                      src={`${IMAGE_BASE_URL}${product.image}`}
+                                      className="h-full w-full object-cover object-center"
+                                    />
+                                  </div>
                                   <p className="font-bold">{product.name}</p>
                                   <dl className="mt-3">
                                     <dt className="pt-3 font-semibold first:pt-0 dark:text-white">
@@ -76,7 +90,7 @@ const OrderList = () => {
                                       Quantity: {product.quantity}
                                     </dt>
 
-                                    <dt className="pt-3 font-semibold first:pt-0 dark:text-white text-red-600">
+                                    <dt className="pt-3 font-semibold text-red-600 first:pt-0 dark:text-white">
                                       Total:{' '}
                                       {formatCurrencyVND(
                                         product.quantity * product.price,
@@ -86,6 +100,10 @@ const OrderList = () => {
                                 </div>
                               </div>
                             ))}
+                          </td>
+
+                          <td className="whitespace-nowrap px-6 py-4 text-sm font-semibold uppercase text-red-600 dark:text-blue-200">
+                            {formatCurrencyVND(data.total)}
                           </td>
                         </tr>
                       ))}
